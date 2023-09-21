eceacd31d51612197449433ad3955f1ab33d52c7
amightybeard <126688479+amightybeard@users.noreply.github.com>
2023-09-20 20:46:53 -0400

    Update nflblogs_post.py

--- nflblogs_post.py
+++ nflblogs_post.py
@@ -26,41 +26,57 @@ def post_to_squabblr(title, content):
     return response.json()
 
 def main():
-    logging.info("Fetching articles data...")
-    response = requests.get(GIST_URL_DETAILS)
-    articles = response.json()
-    logging.info("Article data fetched successfully.")
+    try:
+        logging.info("Fetching articles data...")
+        response = requests.get(GIST_URL_DETAILS)
+        if response.status_code != 200:
+            logging.error(f"Error fetching articles data: {response.status_code}, {response.text}")
+            exit(1)
 
-    # Find the first article that hasn't been posted
-    for article in articles:
-        if not article["posted"]:
-            # Prepare title and content
-            post_title = f"[{article['blog_name']}] {article['title']}"
-            post_content = f"Check out [{article['title']}]({article['url']}) by {article['blog_name']}\n\n-----\n\nI'm a bot. Post feedback, blog inclusion requests, and suggestions to /s/ModBot in [the announcement post](https://squabblr.co/u/modbot/post/G8wA45APxz)."
+        articles = response.json()
+        logging.info("Article data fetched successfully.")
 
-            # Post to Squabblr.co
-            post_to_squabblr(post_title, post_content)
+        # Find the first article that hasn't been posted
+        for article in articles:
+            if not article["posted"]:
+                # Prepare title and content
+                post_title = f"[{article['blog_name']}] {article['title']}"
+                post_content = f"Check out [{article['title']}]({article['url']}) by {article['blog_name']}\n\n-----\n\nI'm a bot. Post feedback, blog inclusion requests, and suggestions to /s/ModBot in [the announcement post](https://squabblr.co/u/modbot/post/G8wA45APxz)."
 
-            # Update the article's "posted" status
-            article["posted"] = True
+                # Post to Squabblr.co
+                post_response = post_to_squabblr(post_title, post_content)
+                if 'error' in post_response:
+                    logging.error(f"Error posting article: {post_response['error']}")
+                    exit(1)
+                logging.info(f"Article '{post_title}' posted successfully.")
 
-            logging.info(f"Updating nflblogs-article-details.json to mark '{article['title']}' as posted...")
-            # Update nflblogs-article-details.json gist with the updated article
-            headers = {
-                "Authorization": f"token {GIST_TOKEN}",
-                "Accept": "application/vnd.github.v3+json"
-            }
-            payload = {
-                "files": {
-                    FILE_NAME_DETAILS: {
-                        "content": json.dumps(articles, indent=4)
+                # Update the article's "posted" status
+                article["posted"] = True
+
+                logging.info(f"Updating nflblogs-article-details.json to mark '{article['title']}' as posted...")
+                # Update nflblogs-article-details.json gist with the updated article
+                headers = {
+                    "Authorization": f"token {GIST_TOKEN}",
+                    "Accept": "application/vnd.github.v3+json"
+                }
+                payload = {
+                    "files": {
+                        FILE_NAME_DETAILS: {
+                            "content": json.dumps(articles, indent=4)
+                        }
                     }
                 }
-            }
-            response = requests.patch(f"https://api.github.com/gists/{GIST_ID_DETAILS}", headers=headers, json=payload)
-            logging.info(f"'{article['title']}' marked as posted successfully.")
-            
-            break  # Exit the loop after posting one article
+                response = requests.patch(f"https://api.github.com/gists/{GIST_ID_DETAILS}", headers=headers, json=payload)
+                if response.status_code != 200:
+                    logging.error(f"Error updating nflblogs-article-details.json: {response.status_code}, {response.text}")
+                    exit(1)
+                logging.info(f"'{article['title']}' marked as posted successfully.")
+                
+                break  # Exit the loop after posting one article
+    except Exception as e:
+        logging.exception(f"An unexpected error occurred: {e}")
+
+logging.info("Bot completed.")
 
 if __name__ == "__main__":
     main()
