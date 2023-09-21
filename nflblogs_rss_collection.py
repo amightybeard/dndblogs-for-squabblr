78fe01a05a5a477f4fde2f3dcb9ec72a7446158f
amightybeard <126688479+amightybeard@users.noreply.github.com>
2023-09-21 13:02:17 -0400

    Update nflblogs_rss_collection.py

--- nflblogs_rss_collection.py
+++ nflblogs_rss_collection.py
@@ -6,6 +6,7 @@ import requests
 import feedparser
 import logging
 import re
+import html
 from datetime import datetime, timezone
 from dateutil import parser
 
@@ -22,16 +23,24 @@ FILE_NAME_DETAILS = 'nflblogs-article-details.json'
 GIST_URL_TRACKER = f"https://gist.githubusercontent.com/amightybeard/{GIST_ID_TRACKER}/raw/{FILE_NAME_TRACKER}"
 GIST_URL_DETAILS = f"https://gist.githubusercontent.com/amightybeard/{GIST_ID_DETAILS}/raw/{FILE_NAME_DETAILS}"
 
-def parse_date(date_str):
+def parse_date_to_datetime(date_str):
+    """
+    Parse a date string using dateutil's parser.
+    If the datetime is offset-naive, default to UTC.
+    Returns a datetime object.
+    """
+    dt = parser.parse(date_str)
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=timezone.utc)  # Default to UTC
+    return dt
+
+def parse_date_to_iso(date_str):
     """
     Parse a date string using dateutil's parser.
     If the datetime is offset-naive, default to UTC.
     Returns date in ISO format.
     """
-    dt = parser.parse(date_str)
-    if dt.tzinfo is None:
-        dt = dt.replace(tzinfo=timezone.utc)  # Default to UTC
-    return dt.isoformat()
+    return parse_date_to_datetime(date_str).isoformat()
 
 logging.info("Fetching tracker data...")
 response = requests.get(GIST_URL_TRACKER)
@@ -48,19 +57,24 @@ for blog in rss_tracker_data["blogs"]:
     feed = feedparser.parse(blog["rss_url"])
     for entry in feed.entries:
         article_date_str = entry.published.split("T")[0] if "T" in entry.published else entry.published
-
+        
         if article_date_str:
-            article_date = parse_date(article_date_str)
+            article_date = parse_date_to_datetime(article_date_str)
         else:
             continue  # Skip this entry and move to the next
-
         if article_date > last_fetched_date:
+            description_cleaned = re.sub('<[^<]+?>', '', entry.get("description", ""))  # Remove HTML tags
+            description_cleaned = html.unescape(description_cleaned)  # Convert HTML entities to their respective characters
+            description_cleaned = description_cleaned.replace("&nbsp;", " ")  # Replace non-breaking spaces with regular spaces
+            description_cleaned = re.sub(' +', ' ', description_cleaned)  # Replace multiple spaces with a single space
+            description_cleaned = description_cleaned.strip()  # Remove leading and trailing whitespaces
+
             new_articles.append({
                 "blog_name": blog["blog_name"],
                 "url": entry.link,
                 "title": entry.title,
-                "description": re.sub('<[^<]+?>', '', entry.get("description", "")),
-                "date_published": entry.published,
+                "description": description_cleaned,
+                "date_published": parse_date_to_iso(article_date_str),
                 "posted": False
             })
 logging.info(f"RSS feed parsing completed. Found {len(new_articles)} new articles.")
