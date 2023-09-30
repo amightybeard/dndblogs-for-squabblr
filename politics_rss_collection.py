11be313f5143ef65d530f375ebec75bd99ec30f1
amightybeard <126688479+amightybeard@users.noreply.github.com>
2023-09-30 17:35:15 -0400

    Update politics_rss_collection.py

--- politics_rss_collection.py
+++ politics_rss_collection.py
@@ -120,7 +120,7 @@ def write_to_json(data, file_name):
 def main():
     # URLs of the RSS feeds
     rss_urls = [
-        ('https://www.govtrack.us/events/events.rss?list_id=2xtKwzEbrPGqdftV', 'Activity')
+        ('https://www.govtrack.us/events/events.rss?list_id=2xtKwzEbrPGqdftV', 'Activity'),
         # ('https://www.govtrack.us/events/events.rss?list_id=bIEEeNizAdvQ12hc', 'Votes'),
         ('https://www.govtrack.us/events/events.rss?list_id=jjfjQNLQe3meewpG', 'New')
     ]
