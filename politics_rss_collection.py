fad07a1bf4b5bd32246aebfa90b412e4767dbdcd
amightybeard <126688479+amightybeard@users.noreply.github.com>
2023-11-04 14:27:28 -0400

    Update politics_rss_collection.py

--- politics_rss_collection.py
+++ politics_rss_collection.py
@@ -157,7 +157,7 @@ def main():
     # URLs of the RSS feeds
     rss_urls = [
         ('https://www.govtrack.us/events/events.rss?list_id=2xtKwzEbrPGqdftV', 'Activity'),
-        ('https://www.govtrack.us/events/events.rss?list_id=bIEEeNizAdvQ12hc', 'Votes'),
+        # ('https://www.govtrack.us/events/events.rss?list_id=bIEEeNizAdvQ12hc', 'Votes'),
         ('https://www.govtrack.us/events/events.rss?list_id=jjfjQNLQe3meewpG', 'New')
     ]
     all_items = []
